3cbe35bbbc25c5463ef9ec39e897287646e75b97
timibadass <omoyenitimi@gmail.com>
2023-05-11 15:05:03 +0100

    init

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import EventService from "@/services/EventService.js";
+  import { computed, onMounted, ref } from "vue";
+
+  const props = defineProps(["id"]);
+
+  const event = ref("");
+  const id = computed(() => props.id);
+  onMounted(() => {
+    EventService.getEvent(id.value)
+      .then((response) => {
+        event.value = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import EventCard from "@/components/EventCard.vue";
+  import EventService from "@/services/EventService.js";
+  import { onMounted, ref } from "vue";
+
+  const events = ref("");
+
+  onMounted(() => {
+    EventService.getEvents()
+      .then((response) => {
+        events.value = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
