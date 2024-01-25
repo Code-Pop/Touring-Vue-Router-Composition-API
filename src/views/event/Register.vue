812241e325eb32a8c8121c32dd81b08cccaece4a
4YourCompany <77233863+4YourCompany@users.noreply.github.com>
2024-01-25 17:09:42 +0100

    zmiana4

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { defineProps } from "vue";
+const register = () => {
+  console.log("registering for event");
+};
 
 defineProps(["event"]);
 </script>
 <template>
+  <div>
   <p>Register for the event here</p>
+  <button @click="register">register me!</button>
+</div>
 </template>
